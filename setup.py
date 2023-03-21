efcf284acebc05eac3d86c3f6eb0a024e846415e
Eragod <ernure223@gmail.com>
2023-03-21 11:09:13 +0600

    0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
     name='ErCore',
-    version='0.1',
+    version='0.2',
     packages=['ErCore'],
-    entry_points={
-        'console_scripts': [
-            'ErCore = ErCore.cli:make_project_command'
-        ]
-    }
+    author='Eragod',
+    description='This is a core for my package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={'console_scripts': ['ErCore = ErCore.cli:make_project_command']}
 )

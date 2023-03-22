99f4be689d166edb885d291e1aaa3a4450a13bb3
Eragod <ernure223@gmail.com>
2023-03-22 14:23:00 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-with open('README.md', 'r') as readme:
-    long_description = readme.read()
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
 
-with open('ErCore/VERSION', 'r') as version:
-    ver = version.read()
+with open('ErCore/VERSION', 'r') as version_file:
+    version = version_file.read().strip()
 
 setup(
     name='ErCore',
-    version=ver,
+    version=version,
     packages=['ErCore'],
     author='Eragod',
     description='This is a core for my package',
@@ -16,4 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     entry_points={'console_scripts': ['ErCore = ErCore.cli:make_project_command']},
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent'],
 )
